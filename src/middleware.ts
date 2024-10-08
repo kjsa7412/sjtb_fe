341153b068bdf8a1af395cab1da593205f880ad8
오병주 <obj98@sjinc.co.kr>
2024-10-08 17:35:22 +0900

    미들웨어 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,20 +10,32 @@ export const config = {
 export function middleware(request: NextRequest) {
     const authToken = request.cookies.get(EJWT.AccessToken)?.value || '';
 
+
+    console.log('authToken ++++ ' + authToken)
+
     const originalPathname = request.nextUrl.pathname;
     const newPathname = originalPathname.startsWith('/APICALL')
         ? originalPathname.substring('/APICALL'.length)
         : originalPathname;
+
+    // if (authToken) {
+    //     request.headers.set('Authorization', `Bearer ${authToken}`);
+    // }
+
+
 
     // API 주소로 프록시
     const response = NextResponse.rewrite(
         new URL(`${process.env.NEXT_PUBLIC_REAL_SVR_BASE_URL}${newPathname}${request.nextUrl.search}`, request.url)
     );
 
-    // AccessToken이 있는 경우 할당
-    // if (authToken) {
-    //     response.headers.set('Authorization', `Bearer ${authToken}`);
-    // }
+    console.log('response___________')
+    console.log(response)
+
+    // // AccessToken이 있는 경우 할당
+    if (authToken) {
+        response.headers.set('Authorization', `Bearer ${authToken}`);
+    }
 
     return response;
 }
