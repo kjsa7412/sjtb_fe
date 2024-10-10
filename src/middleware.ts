62a8c1effefdc90978c62f789b2d5b55028737e0
오병주 <obj98@sjinc.co.kr>
2024-10-10 15:37:55 +0900

    미들웨어 수정 최종

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
-import { NextResponse, type NextRequest } from "next/server";
+import {type NextRequest, NextResponse} from "next/server";
 
-import { EJWT } from "@/types/enums/common-enum";
+import {EJWT} from "@/types/enums/common-enum";
 
 // 프록시 조건
 export const config = {
@@ -10,15 +10,10 @@ export const config = {
 export function middleware(request: NextRequest) {
     const authToken = request.cookies.get(EJWT.AccessToken)?.value || '';
 
-
-    console.log('authToken ++++ ' + authToken)
-
     const originalPathname = request.nextUrl.pathname;
     const newPathname = originalPathname.startsWith('/APICALL')
         ? originalPathname.substring('/APICALL'.length)
         : originalPathname;
-
-
 
     // 새로운 요청 헤더 설정
     const requestHeaders = new Headers(request.headers);
@@ -29,7 +24,7 @@ export function middleware(request: NextRequest) {
     }
 
     // API 주소로 프록시하면서 새로운 요청 헤더 설정
-    const response = NextResponse.rewrite(
+    return NextResponse.rewrite(
         new URL(`${process.env.NEXT_PUBLIC_REAL_SVR_BASE_URL}${newPathname}${request.nextUrl.search}`, request.url),
         {
             request: {
@@ -37,18 +32,4 @@ export function middleware(request: NextRequest) {
             },
         }
     );
-
-    console.log('response___________')
-    console.log(response)
-
-    // // AccessToken이 있는 경우 할당
-    // if (authToken) {
-    //     response.headers.set('Authorization', `Bearer ${authToken}`);
-    // }
-
-
-    console.log('response__________22222222222222_')
-    console.log(response)
-
-    return response;
 }
