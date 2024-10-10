63227b0f48f04abf62de571574bc8915cf327bb4
Kyungjin Sa <kjsa7412@gmail.com>
2024-10-11 01:14:05 +0900

    write 수정

--- src/app/board/new/page.tsx
+++ src/app/board/new/page.tsx
@@ -1,15 +1,47 @@
+'use client'
+
+import {useState} from "react";
+
 import {EBlank} from "@/types/enums/common-enum";
 
 import PageContainer from "@/components/containers/PageContainer";
 import Blank from "@/components/blank/Blank";
+import BodyContainer from "@/components/containers/BodyContainer";
+import ContentsContainer from "@/components/containers/ContentsContainer";
+
+
+const TitleInput = ({title, setTitle}: { title: string; setTitle: (value: string) => void }) => {
+    return (
+        <input
+            type="text"
+            placeholder="Enter title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            style={{
+                width: "100%",
+                fontSize: "42px",
+                marginTop: "20px",
+                boxSizing: "border-box",
+            }}
+        />
+    );
+};
 
 const New = () => {
-    return(
+    const [title, setTitle] = useState("");
+
+
+    return (
         <PageContainer>
             <Blank type={EBlank.Header}/>
-            New
+            <BodyContainer>
+                <ContentsContainer>
+                    <Blank type={EBlank.Column} size={60}/>
+                    <TitleInput title={title} setTitle={setTitle}/>
+                </ContentsContainer>
+            </BodyContainer>
         </PageContainer>
-    )
-}
+    );
+};
 
 export default New;
