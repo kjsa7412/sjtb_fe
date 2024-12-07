38bf224c901f13e1ecd11b2090e2240541f4b067
kjsa <kjsa7412@gmail.com>
2024-12-07 22:42:36 +0900

    팝업 수정
    - 화면 이동 시 팝업이 그대로 열려져 있는 문제 수정

--- src/components/containers/PageContainer.tsx
+++ src/components/containers/PageContainer.tsx
@@ -7,6 +7,7 @@ import {useEffect, useState} from "react";
 import {ILogin, IUser} from "@/types/interfaces/common-interface";
 import useActionAndNavigate from "@/hooks/useActionAndNavigate";
 import {loginAtom} from "@/atoms/loginAtom";
+import usePopup from "@/hooks/usePopup";
 
 import styles from "./PageContainer.module.scss";
 
@@ -19,16 +20,22 @@ const PageContainer = ({children}: Props) => {
     const [rcLogin] = useRecoilState<ILogin>(loginAtom);
     const pathName = usePathname();
     const actionAndNavigate = useActionAndNavigate();
+    const popupController = usePopup();
 
     useEffect(() => {
         setIsAuth(false);
         if (!rcLogin.isLogin && (pathName === '/board/new' || /^\/board\/\d+\/edit$/.test(pathName))) {
             actionAndNavigate.actionAndNavigate('/')
-        }
-        else {
+        } else {
             setIsAuth(true);
         }
     }, [pathName, rcLogin]);
+
+    useEffect(() => {
+        return () => {
+            popupController.closeAll();
+        };
+    },[])
 
     return (
         <div className={styles.baseContainer}>{isAuth && children}</div>
